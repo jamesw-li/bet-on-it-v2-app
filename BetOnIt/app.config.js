4c871e4f63b9a9dbbefe80c040d00ee20b645d78
jamesw-li <jamesli788@gmail.com>
2025-09-11 20:14:30 -0700

    Updated app.config.js

--- BetOnIt/app.config.js
+++ BetOnIt/app.config.js
@@ -28,10 +28,6 @@ export default {
     web: {
       favicon: './assets/favicon.png'
     },
-    extra: {
-      supabaseUrl: process.env.SUPABASE_URL,
-      supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
-    },
     plugins: [
       'expo-notifications'
     ]
